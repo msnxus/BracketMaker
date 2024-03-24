53a1473976009ccf36c5ba93f3920237ea8a333a
Nicolas Nee <nn3965@princeton.edu>
2024-03-24 16:36:11 -0400

    get_bracket

--- src/database/test.py
+++ src/database/test.py
@@ -2,11 +2,14 @@ import os
 import sys
 import psycopg2
 from create_bracket import create_bracket
+from get_bracket_from_code import get_bracket_from_code
 
 DATABASE_URL = "dbname='bracket' user='nn3965' host='localhost' password='4234'"
 
-def main():
+def test_create_bracket():
     create_bracket(1234, "Super Smash Bros", ["Nico", "Billy", "Ryan"])
+
+def fetch_all_info():
     try:
         with psycopg2.connect(DATABASE_URL) as connection:
             with connection.cursor() as cursor:
@@ -17,6 +20,15 @@ def main():
     except Exception as ex:
         print(ex, file=sys.stderr)
         sys.exit(1)
+        
+def test_get_bracket():
+    get_bracket_from_code(1234)
+
+def main():
+    # test_create_bracket()
+    # fetch_all_info()
+    test_get_bracket()
+    
         
 if __name__ == '__main__':
     main()
