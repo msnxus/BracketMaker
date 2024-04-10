390d96298b78361fdd3700b957e588fc8e162e51
Ryan Hoffman <rh8490@princeton.edu>
2024-04-10 11:20:43 -0400

    added new render postgres url

--- src/database.py
+++ src/database.py
@@ -5,8 +5,8 @@ import psycopg2
 # You will need to set up 
 
 # DATABASE_URL = "dbname='bracket' user='nn3965' host='localhost' password='4234'"
-DATABASE_URL = "dbname='bracket' user='postgres' host='localhost' password='cos333'"
-
+# DATABASE_URL = "dbname='bracket' user='postgres' host='localhost' password='cos333'"
+DATABASE_URL = 'postgres://bracket_sa3u_user:zIWzQ9iIrc21F0EVdRTheCpNZ23nX6Fi@dpg-cobap5779t8c73br7rig-a/bracket_sa3u'
 
 def initialize():
 
